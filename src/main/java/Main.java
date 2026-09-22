51b5a0770fe1e19c33871663db31dea2c1a25c7b
Pavlo I <ivchenko.pa@gmail.com>
2026-09-22 16:32:04 +0200

    - implement user interaction logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     static void main(String[] args) {
-        System.out.println("Hello World, please provide your password: ");
+        Scanner scanner = new Scanner(System.in);
+        boolean isPasswordValid = false;
+
+        while (!isPasswordValid){
+            System.out.println("Please enter password: \n" +
+                    "Hint: should be min 8 characters long, should contain at least one uppercase, one lowercase and a digit");
+            String password = scanner.nextLine();
+            isPasswordValid = PasswordValidator.isValid(password);
+        }
     }
 
 }
